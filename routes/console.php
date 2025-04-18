204229d726415b7e18ee4fe29147d6ca71c498e5
mario landa <mlandacoding@gmail.com>
2025-04-17 22:14:12 -0500

    successfully scheduled a job and added logging

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+
+Schedule::command('python:update-stocks-overview')
+    ->dailyAt('03:45')
+    ->timezone('America/New_York')
+    ->sendOutputTo(storage_path('logs/update-stocks.log'));
