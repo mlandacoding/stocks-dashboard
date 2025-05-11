a8f5977923fe21b546425e475527553f64ce3c51
mario landa <mlandacoding@gmail.com>
2025-05-11 02:06:50 -0500

    fixed syntax

--- python/populateFinancials.py
+++ python/populateFinancials.py
@@ -137,7 +137,7 @@ for symbol in stocks:
         print(filing)
         cik = filing.cik
         filing_id = insert_filing(cursor, filing, symbol, cik)
-        if(filing_id == 'EXISTS' or filing_id == 'ERROR':
+        if filing_id == 'EXISTS' or filing_id == 'ERROR':
             print(f'Filing - {filing.timeframe} for {symbol} already exists or contains no timeframe')
             continue
 
