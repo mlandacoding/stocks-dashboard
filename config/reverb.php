6fca73e4330b13f6076ebffe5bc50f17de7816b3
mario landa <mlandacoding@gmail.com>
2025-05-14 22:09:52 -0500

    Revert "commands fixed"
    This reverts commit 0619466b13726589a4a99414d4f64c0852e1e687.

--- config/reverb.php
+++ config/reverb.php
@@ -28,32 +28,7 @@ return [
 
     'servers' => [
 
-        'reverb' => env('APP_ENV') === 'local' ?
-        [
-            'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
-            'port' => env('REVERB_SERVER_PORT', 8080),
-            'path' => env('REVERB_SERVER_PATH', ''),
-            'hostname' => env('REVERB_HOST'),
-            'options' => [
-            ],
-            'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 50_000),
-            'scaling' => [
-                'enabled' => env('REVERB_SCALING_ENABLED', false),
-                'channel' => env('REVERB_SCALING_CHANNEL', 'reverb'),
-                'server' => [
-                    'url' => env('REDIS_URL'),
-                    'host' => env('REDIS_HOST', '127.0.0.1'),
-                    'port' => env('REDIS_PORT', '6379'),
-                    'username' => env('REDIS_USERNAME'),
-                    'password' => env('REDIS_PASSWORD'),
-                    'database' => env('REDIS_DB', '0'),
-                    'timeout' => env('REDIS_TIMEOUT', 60),
-                ],
-            ],
-            'pulse_ingest_interval' => env('REVERB_PULSE_INGEST_INTERVAL', 15),
-            'telescope_ingest_interval' => env('REVERB_TELESCOPE_INGEST_INTERVAL', 15),
-        ] :
-        [
+        'reverb' => [
             'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
             'port' => env('REVERB_SERVER_PORT', 8080),
             'path' => env('REVERB_SERVER_PATH', ''),
@@ -77,7 +52,7 @@ return [
             ],
             'pulse_ingest_interval' => env('REVERB_PULSE_INGEST_INTERVAL', 15),
             'telescope_ingest_interval' => env('REVERB_TELESCOPE_INGEST_INTERVAL', 15),
-        ]
+        ],
 
     ],
 
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -18,33 +18,21 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+import Echo from 'laravel-echo';
 
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
+import Pusher from 'pusher-js';
+window.Pusher = Pusher;
 
-// const appEnv = import.meta.env.VITE_APP_ENV;
-// window.Echo = new Echo(
-//     appEnv === 'local'
-//         ?
-//         {
-//             broadcaster: 'reverb',
-//             key: import.meta.env.VITE_REVERB_APP_KEY,
-//             wsHost: window.location.hostname,  // not hardcoded localhost
-//             wsPort: 443,
-//             enabledTransports: ['ws'],
-//         }
-//         :
-//         {
-//         broadcaster: 'reverb',
-//             key: import.meta.env.VITE_REVERB_APP_KEY,
-//             wsHost: window.location.hostname,  // not hardcoded localhost
-//             wsPort: 443,
-//             wssPort: 443,
-//             forceTLS: true,
-//             enabledTransports: ['ws','wss'],
-//         }
-// );
+window.Echo = new Echo({
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: window.location.hostname,  // not hardcoded localhost
+    wsPort: 443,
+    wssPort: 443,
+    forceTLS: true,
+    enabledTransports: ['ws'],
+});
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
