c9769a05669c2a5595d921c5ca8dfdf8c9e5283f
mario landa <mlandacoding@gmail.com>
2025-05-17 15:54:01 -0500

    added options api pull and risk free from fred - successfull

--- python/populateOptionChains.py
+++ python/populateOptionChains.py
@@ -1,20 +1,45 @@
-from polygon import RESTClient
-import pandas as pd
-from polygon.rest.models import OptionContractSnapshot
 
-client = RESTClient("redacted")
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from polygon.rest.models import OptionContractSnapshot, DayOptionContractSnapshot
+from polygon import RESTClient
+from dataclasses import asdict
+from datetime import datetime
+from options_calculations.riskFreeRateFromFred import get_free_risk_rate_from_fred
+import json
+
+load_dotenv(dotenv_path='../.env')
+api_key = os.getenv("POLYGON_API_KEY")
+client = RESTClient(api_key)
+
+def get_active_assets() ->dict:
+    with open('./storage/app/public/cache/active_assets.json', 'r') as f:
+        stocks = json.load(f)
+    return {stock['symbol']: stock['company_name'] for stock in stocks}
 
 options_chain = []
 for o in client.list_snapshot_options_chain(
-    "EVRI",
+    "AAPL",
     params={
         "order": "asc",
-        "limit": 10,
+        "limit": 250,
         "sort": "ticker",
     },
 ):
     options_chain.append(o)
 
+for option in options_chain:
+    day_opt = asdict(option.day)
+    details_opt = asdict(option.details)
+    greeks_truth = asdict(option.greeks)
+    price_truth = day_opt['vwap']
+    days_to_expiry = (datetime.fromisoformat(details_opt['expiration_date']) - datetime.today()).days
+    implied_volatility = option.implied_volatility
+    options_symbol = details_opt['ticker']
+    risk_free_rate = round(get_free_risk_rate_from_fred(days_to_expiry), 4)
+
+    print(1)
 df = pd.DataFrame([OptionContractSnapshot.__dict__ for option in options_chain])
 
 print(options_chain)
