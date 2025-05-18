ef2dc415f5f7bdf97206a381d8bf006a90a4c2ee
mario landa <mlandacoding@gmail.com>
2025-05-17 19:25:46 -0500

    successfully implemented black scholes pricing and greeks
    not ideal for american options, but its a start

---
+++ python/options_calculations/blackScholesModel.py
@@ -0,0 +1,80 @@
+import numpy as np
+import scipy.stats as si
+import scipy as scipy
+
+# This implementation was inspired by the approach described here:
+# https://theaiquant.medium.com/mastering-the-black-scholes-model-with-python-a-comprehensive-guide-to-option-pricing-11af712697b7
+# The code here has been independently written and adapted
+# No LLMS were used to create this
+def price_using_black_sholes(
+    price_of_underlying_asset: float,
+    strike_price: float,
+    years_to_expiry: float,
+    risk_free_interest_rate: float,
+    volatility: float,
+    option_type: str) -> float:
+
+    def d1():
+        return (np.log(price_of_underlying_asset / strike_price) + (risk_free_interest_rate + 0.5 * volatility ** 2) * years_to_expiry) / (volatility * np.sqrt(years_to_expiry))
+
+    def d2():
+        return d1() - volatility * np.sqrt(years_to_expiry)
+
+    def price_a_call():
+        return price_of_underlying_asset * si.norm.cdf(d1(), 0.0, 1.0) - strike_price * np.exp(-risk_free_interest_rate * years_to_expiry) * si.norm.cdf(d2(),0.0, 1.0)
+
+    def price_a_put():
+        return strike_price * np.exp(-risk_free_interest_rate * years_to_expiry) * si.norm.cdf(-d2(), 0.0, 1.0) - price_of_underlying_asset * si.norm.cdf(-d1(), 0.0, 1.0)
+
+    if option_type == 'call':
+        return price_a_call()
+    elif option_type == 'put':
+        return price_a_put()
+    else:
+        raise ValueError("option_type must be 'call' or 'put'")
+
+# This implementation was inspired by the approach described here:
+# https://www.macroption.com/black-scholes-formula/#greeks
+# The code here has been independently written and adapted
+# # No LLMS were used to create this
+def get_greeks_black_scholes(price_of_underlying_asset: float,
+    strike_price: float,
+    years_to_expiry: float,
+    risk_free_interest_rate: float,
+    volatility: float,
+    option_type: str,
+    trading_days = 252) -> dict:
+
+    # Theres no exponent for dividend so keep d1 as is ( the stock may have dividends I just dont have the data
+    q = 0
+    d1 = (np.log(price_of_underlying_asset / strike_price) + (risk_free_interest_rate + 0.5 * volatility ** 2) * years_to_expiry) / (volatility * np.sqrt(years_to_expiry))
+    d2 = d1 - volatility * np.sqrt(years_to_expiry)
+    delta = si.norm.cdf(d1) if option_type == 'call' else si.norm.cdf(d1) - 1
+
+    gamma = np.exp(-q*years_to_expiry)* si.norm.pdf(d1)/(price_of_underlying_asset*volatility*np.sqrt(years_to_expiry))
+
+    # couldnt figure theta out by looking at the formulas, here is the source:
+    # https://www.reddit.com/r/options/comments/17m7bgt/options_greeks_calculations/
+    a = price_of_underlying_asset * volatility * np.exp(-q * years_to_expiry) * si.norm.pdf(d1) / (2 * np.sqrt(years_to_expiry))
+    b_for_calls = risk_free_interest_rate * strike_price * np.exp(-risk_free_interest_rate * years_to_expiry) * scipy.special.ndtr(d2)
+    c_for_calls = q * price_of_underlying_asset * np.exp(-q * years_to_expiry) * si.norm.cdf(d1)
+
+    b_for_puts = risk_free_interest_rate * strike_price * np.exp(-risk_free_interest_rate * years_to_expiry) * si.norm.cdf(-d2)
+    c_for_puts = q * price_of_underlying_asset * np.exp(-q * years_to_expiry) * si.norm.cdf(-d1)
+
+    theta = (
+        (1 / trading_days) * (-a - b_for_calls + c_for_calls)
+        if option_type == 'call'
+        else (1 / trading_days) * (-a + b_for_puts - c_for_puts)
+    )
+
+
+    rho_for_calls = strike_price * years_to_expiry * np.exp(-risk_free_interest_rate * years_to_expiry) * si.norm.cdf(d2)
+    rho_for_puts = -strike_price * years_to_expiry * np.exp(-risk_free_interest_rate * years_to_expiry) * si.norm.cdf(-d2)
+
+    rho = rho_for_calls if option_type == 'call' else rho_for_puts
+
+    # uses normal probability density, NOT cumulative
+    vega = price_of_underlying_asset * np.exp(-q * years_to_expiry) * np.sqrt(years_to_expiry) * si.norm.pdf(d1)
+
+    return {'delta':delta, 'gamma': gamma, 'theta': theta, 'vega': vega}
--- python/populateOptionChains.py
+++ python/populateOptionChains.py
@@ -7,16 +7,34 @@ from polygon import RESTClient
 from dataclasses import asdict
 from datetime import datetime
 from options_calculations.riskFreeRateFromFred import get_free_risk_rate_from_fred
+from options_calculations.blackScholesModel import price_using_black_sholes, get_greeks_black_scholes
 import json
 
 load_dotenv(dotenv_path='../.env')
 api_key = os.getenv("POLYGON_API_KEY")
 client = RESTClient(api_key)
 
+
+
 def get_active_assets() ->dict:
-    with open('./storage/app/public/cache/active_assets.json', 'r') as f:
+    with open('../storage/app/public/cache/active_assets.json', 'r') as f:
         stocks = json.load(f)
     return {stock['symbol']: stock['company_name'] for stock in stocks}
+
+
+
+def get_stocks_latest_prices(stocks: dict, snapshot: list) -> dict:
+    stock_to_price = {}
+    for item in snapshot:
+        if item.ticker not in stocks:
+            continue
+        stock_to_price[item.ticker] = item.day.vwap
+
+    return  stock_to_price
+
+todays_snapshot = client.get_snapshot_all("stocks")
+active_stocks = get_active_assets()
+stocks_with_latest_price = get_stocks_latest_prices(active_stocks, todays_snapshot)
 
 options_chain = []
 for o in client.list_snapshot_options_chain(
@@ -37,8 +55,16 @@ for option in options_chain:
     days_to_expiry = (datetime.fromisoformat(details_opt['expiration_date']) - datetime.today()).days
     implied_volatility = option.implied_volatility
     options_symbol = details_opt['ticker']
+    underlying_asset_price = stocks_with_latest_price[option.underlying_asset.ticker]
     risk_free_rate = round(get_free_risk_rate_from_fred(days_to_expiry), 4)
+    strike_price = option.details.strike_price
+    exercise_style = option.details.exercise_style
+    type_of_option = option.details.contract_type
+    years_to_expiry = days_to_expiry / 252 # number of trading days
 
+    # not ideal for american style calls but a fun exercise
+    black_scholes_pricing = price_using_black_sholes(underlying_asset_price,strike_price,years_to_expiry,risk_free_rate,implied_volatility,type_of_option)
+    greeks_black_scholes = get_greeks_black_scholes(underlying_asset_price,strike_price,years_to_expiry,risk_free_rate,implied_volatility,type_of_option)
     print(1)
 df = pd.DataFrame([OptionContractSnapshot.__dict__ for option in options_chain])
 
