711e2e17e01879ce6b7d62bab452986cb806a88a
mario landa <mlandacoding@gmail.com>
2025-06-12 14:00:53 -0500

    added google analytics to navbar

---
+++ resources/js/components/GoogleAnalytics.vue
@@ -0,0 +1,26 @@
+<script setup>
+if (
+  import.meta.env.MODE === 'production' &&
+  import.meta.env.VITE_GOOGLE_ANALYTICS_ID &&
+  !window.gtagInitialized
+) {
+  const id = import.meta.env.VITE_GOOGLE_ANALYTICS_ID
+
+  const script1 = document.createElement('script')
+  script1.async = true
+  script1.src = `https://www.googletagmanager.com/gtag/js?id=${id}`
+
+  const script2 = document.createElement('script')
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${id}');
+  `
+
+  document.head.appendChild(script1)
+  document.head.appendChild(script2)
+
+  window.gtagInitialized = true
+}
+</script>
